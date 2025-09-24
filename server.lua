f02ad2e6a41ee8e3548783cc2b979efcd370dc6a
Carson <143346310+TheStoicCarson@users.noreply.github.com>
2025-09-23 21:33:30 -0400

    Readded Support for Scully Menu

--- server.lua
+++ server.lua
@@ -1,7 +1,7 @@
 AddEventHandler("nex-duty:server:onDutyStart", function(source, callsign, entity_id)
 
     -- Set the 'onduty' state from the server safely
---    Player(source).state:set("onduty", true, true)
+    Player(source).state:set("onduty", true, true)
     TriggerClientEvent('dutybridge:client:giveArmor', source)
     TriggerClientEvent('cs:bodycam:useBodyCam', source)
     Wait(500)
@@ -10,7 +10,8 @@ end)
 
 AddEventHandler("nex-duty:server:onDutyEnd", function(source, entity_id)
 
---    Player(source).state:set("onduty", false, true)
+    Player(source).state:set("onduty", false, true)
     TriggerClientEvent('cs:bodycam:useBodyCam', source)
+    Wait(500)
     TriggerClientEvent('okokNotify:Alert', source, 'Duty System', 'You are now off duty.', 3000, 'info', true)
 end)
